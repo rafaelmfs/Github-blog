edb255656eaaf19b355f0608274f7ebd6acffaa4
Rafael Marques <80429145+rafaelmfs@users.noreply.github.com>
2022-10-16 18:27:53 -0300

    Update search.ts

--- src/api/search.ts
+++ src/api/search.ts
@@ -3,7 +3,7 @@ import { apiSearch } from './api'
 export async function search(user: string, repo: string, query = '') {
   const request = await apiSearch.get('issues', {
     params: {
-      q: `${query}repo:${user}/${repo}`,
+      q: `${query}is:issue repo:${user}/${repo}/`,
     },
   })
 
