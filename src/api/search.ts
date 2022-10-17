2507e2929858750cb715303a11901e1ed96b565f
Rafael Marques <80429145+rafaelmfs@users.noreply.github.com>
2022-10-17 09:32:08 -0300

    Update search.ts

--- src/api/search.ts
+++ src/api/search.ts
@@ -4,6 +4,8 @@ export async function search(user: string, repo: string, query = '') {
   const request = await apiSearch.get('issues', {
     params: {
       q: `${query}is:issue repo:${user}/${repo}/`,
+      sort: 'created',
+      order: 'desc',
     },
   })
 
